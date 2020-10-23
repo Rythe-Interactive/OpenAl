01f83caf4cf15eed85ec2d0dfd6f6ebfb539434c
Jelle Vrieze <jelle.vrieze@gmail.com>
2020-10-23 14:32:36 +0200

    [ADD] read_file function

--- OpenAL/wav_loader.hpp
+++ OpenAL/wav_loader.hpp
@@ -1,9 +1,12 @@
 #pragma once
+#pragma warning(disable : 4996)
+#define _CRT_SECURE_NO_WARNINGS 
 #include <cstdint>
 #include <iostream>
 #include <fstream>
 #include <array>
 #include <vector>
+#include <cassert>
 
 typedef int32_t int32;
 typedef int16_t int16;
@@ -12,6 +15,7 @@ typedef uint8_t uint8;
 typedef const char* cstring;
 
 bool compare(uint8 arr[], cstring string, int size);
+inline std::vector<uint8_t> read_file(cstring path);
 
 /**
 * Structs for storing WAV information
@@ -50,34 +54,7 @@ void load_wav(cstring filePath, unsigned char** data, int* size, int* freq)
 	WAVE_Data wave_data;
 	std::vector<uint8> fileData;
 
-	{
-		std::ifstream file;
-		file.open(filePath, std::ifstream::in | std::ifstream::binary);
-		if (!file)
-		{
-			std::cout << "Failed to load " << filePath << std::endl;
-			return;
-		}
-
-		std::cout << "Loading audio file " << filePath << "..." << std::endl;
-
-		// Load all the file into fileData;
-		int loops = 0;
-		while (!file.eof())
-		{
-			char* buffer = new char[1];
-			file.read(buffer, 1);
-			fileData.push_back(*buffer);
-			++loops;
-			delete buffer;
-		}
-		std::cout << "file loops: " << loops << std::endl;
-		file.close();
-
-		file.open(filePath, std::ifstream::ate);
-		std::cout << "size " << file.tellg() << std::endl;
-		file.close();
-	}
+	fileData = read_file(filePath);
 
 	riff_header.chunckId[0] = fileData[0];
 	riff_header.chunckId[1] = fileData[1];
@@ -137,27 +114,11 @@ void load_wav(cstring filePath, unsigned char** data, int* size, int* freq)
 	memcpy(&wave_data.subChunck2Size, (void*)&fileData[40], 4);
 	std::cout << "subChunck2Size: " << wave_data.subChunck2Size << std::endl;
 
-	if (true)
-	{
-		*data = new unsigned char[fileData.size() - 45];
-		memcpy(*data, (void*)&fileData[44], fileData.size() - 45);
-		/*for (int i = 0; i < fileData.size() - 44; ++i)
-		{
-			data[i] = fileData[44 + i];
-			std::cout << "[" << i << "] " << data[i] << std::endl;
-		}*/
-		std::cout << "File Data size: " << fileData.size()-45 << std::endl;
-		*size = (fileData.size() - 45);
-		//std::cout << data << std::endl;
-	}
-	/*else
-	{
-		data = new unsigned char[wave_data.subChunck2Size];
-		FILE* file;
-		fopen_s(&file, filePath, "rb");
-		fread(data, wave_data.subChunck2Size, 1, file);
-		*size = wave_data.subChunck2Size;
-	}*/
+	*data = new unsigned char[fileData.size() - 44];
+	memcpy(*data, (void*)&fileData[44], fileData.size() - 44);
+	std::cout << "File Data size: " << fileData.size()-44 << std::endl;
+	*size = (fileData.size() - 44);
+
 	*freq = wave_format.sampleRate;
 	std::cout << "Loaded audio file!" << std::endl;
 }
@@ -169,4 +130,25 @@ bool compare(uint8 arr[], cstring string, int size)
 		if (arr[i] != string[i]) return false;
 	}
 	return true;
+}
+
+inline std::vector<uint8_t> read_file(cstring path)
+{
+	//create managed FILE ptr
+	const std::unique_ptr<FILE, decltype(&fclose)> file(
+		fopen(std::string(path).c_str(), "r+b"),
+		fclose // deleter is fclose
+	);
+
+	assert("could not open file", file);
+
+	//get size and create container of that size
+	fseek(file.get(), 0L, SEEK_END);
+	std::vector<uint8_t> container(ftell(file.get()));
+	fseek(file.get(), 0L, SEEK_SET);
+
+	//read file 
+	fread(container.data(), sizeof(unsigned char), container.size(), file.get());
+
+	return container;
 }
